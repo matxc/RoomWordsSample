5f1b1b58e1a6bab56aed044b7662a02ff71d0028
Lancelot222 <mcyran2@gmail.com>
2020-02-16 09:12:54 -0600

    Android fundamentals 10.1 Part B: Deleting data from a Room database

---
+++ app/src/main/java/com/example/roomwordssample/Word.java
@@ -0,0 +1,42 @@
+package com.example.roomwordssample;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.room.ColumnInfo;
+import androidx.room.Dao;
+import androidx.room.Entity;
+import androidx.room.Insert;
+import androidx.room.PrimaryKey;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public Word(@NonNull String word) {
+        this.mWord = word;
+    }
+
+    public String getWord() {
+        return this.mWord;
+    }
+
+    @Dao
+    public interface WordDao {
+
+        @Insert
+        void insert(Word word);
+
+        @Query("DELETE FROM word_table")
+        void deleteAll();
+
+        @Query("SELECT * from word_table ORDER BY word ASC")
+        LiveData<List<Word>> getAllWords();
+    }
+}
